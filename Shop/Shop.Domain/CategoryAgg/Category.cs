a9f065eecadcd1ac555c578951d67f459f8acaad
Arhamepa <PC@DESKTOP-PQA26UF>
2024-12-09 12:26:31 +0330

    Domain Is Compeleted

---
+++ Shop/Shop.Domain/CategoryAgg/Category.cs
@@ -0,0 +1,49 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+using Common.Domain.Utilities;
+using Common.Domain.ValueObjects;
+using Shop.Domain.CategoryAgg.Service;
+
+namespace Shop.Domain.CategoryAgg;
+
+public class Category:AggregateRoot
+{
+    public string Title { get; private set; }
+    public string Slug { get; private set; }
+    public SeoData SeoData { get; private set; }
+    public long? ParentId { get; private set; }
+    public List<Category> Children { get; private set; }
+    public Category(string title, string slug, SeoData seoData, ICategoryDomainService service)
+    {
+        slug = slug?.ToSlug();
+        Guard(title, slug, service);
+        Title = title;
+        Slug = slug;
+        SeoData = seoData;
+    }
+    public void Edit(string title, string slug, SeoData seoData, ICategoryDomainService service)
+    {
+        slug = slug?.ToSlug();
+        Guard(title, slug, service);
+        Title = title;
+        Slug = slug;
+        SeoData = seoData;
+    }
+
+    public void AddChildren(string title, string slug, SeoData seoData, ICategoryDomainService service)
+    {
+        Children.Add(new Category(title,slug,seoData,service)
+        {
+            ParentId = Id
+        });
+    }
+    public void Guard(string title, string slug,ICategoryDomainService service)
+    {
+        NullOrEmptyDomainDataException.CheckString(title, nameof(title));
+        NullOrEmptyDomainDataException.CheckString(slug, nameof(slug));
+
+        if (slug != Slug)
+            if (service.SlugIsExist(slug.ToSlug()))
+                throw new SlugIsDuplicateException();
+    }
+}
---
+++ Shop/Shop.Domain/CommentAgg/Comment.cs
@@ -0,0 +1,42 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+
+namespace Shop.Domain.CommentAgg;
+
+public class Comment:AggregateRoot
+{
+
+    public long UserId { get; private set; }
+    public long ProductId { get; private set; }
+    public string Text { get; private set; }
+    public CommentStatus Status { get; private set; }
+    public DateTime UpdateDateTime { get; private set; }
+
+    public Comment(string text, CommentStatus status, long userId, long productId)
+    {
+        NullOrEmptyDomainDataException.CheckString(text, nameof(text));
+        Text = text;
+        Status = CommentStatus.Pending;
+        UserId = userId;
+        ProductId = productId;
+    }
+
+    public void Edit(string text)
+    {
+        NullOrEmptyDomainDataException.CheckString(text, nameof(text));
+        Text = text;
+        UpdateDateTime = DateTime.Now;
+    }
+
+    public void ChangeStatus(CommentStatus status)
+    {
+        UpdateDateTime = DateTime.Now;
+        Status = status;
+    }
+    public enum CommentStatus 
+    {
+        Pending,
+        Accepted,
+        Rejected,
+    }
+}
