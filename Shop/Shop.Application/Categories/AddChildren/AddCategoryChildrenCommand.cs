48c0ebfb69ed3c640c6ea176e245b3a59c32bed4
Arhamepa <PC@DESKTOP-PQA26UF>
2024-12-11 16:19:02 +0330

    UseCase of Category is Done

---
+++ Shop/Shop.Application/Categories/AddChildren/AddCategoryChildrenCommand.cs
@@ -0,0 +1,6 @@
+﻿using Common.Application;
+using Common.Domain.ValueObjects;
+
+namespace Shop.Application.Categories.AddChildren;
+
+public record AddCategoryChildrenCommand(long ParentId, string Title, string Slug, SeoData SeoData) : IBaseCommand;
---
+++ Shop/Shop.Application/Categories/AddChildren/AddCategoryChildrenCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Common.Application;
+using Common.Application.Validation;
+using FluentValidation;
+using Shop.Domain.CategoryAgg.Repository;
+using Shop.Domain.CategoryAgg.Service;
+
+namespace Shop.Application.Categories.AddChildren;
+
+public class AddCategoryChildrenCommandHandler : IBaseCommandHandler<AddCategoryChildrenCommand>
+{
+    private readonly ICategoryRepository _repository;
+    private readonly ICategoryDomainService _domainService;
+
+    public AddCategoryChildrenCommandHandler(ICategoryRepository repository, ICategoryDomainService domainService)
+    {
+        _repository = repository;
+        _domainService = domainService;
+    }
+
+    public async Task<OperationResult> Handle(AddCategoryChildrenCommand request, CancellationToken cancellationToken)
+    {
+        var category = await _repository.GetTracking(request.ParentId);
+        if (category == null)
+            return OperationResult.NotFound();
+        category.AddChildren(request.Title, request.Slug, request.SeoData, _domainService);
+        return OperationResult.Success();
+    }
+
+
+  
+}
