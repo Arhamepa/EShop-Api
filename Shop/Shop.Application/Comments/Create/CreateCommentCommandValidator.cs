bbf91ad9b823d77a2bb8cdfef0e8ed307e4bf113
Arhamepa <PC@DESKTOP-PQA26UF>
2024-12-15 00:06:47 +0330

    Comment Command is Done

---
+++ Shop/Shop.Application/Comments/Create/CreateCommentCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using Common.Application.Validation;
+using FluentValidation;
+
+namespace Shop.Application.Comments.Create;
+
+public class CreateCommentCommandValidator:AbstractValidator<CreateCommentCommand>
+{
+    public CreateCommentCommandValidator()
+    {
+        RuleFor(t => t.Text)
+            .NotNull()
+            .MaximumLength(5).WithMessage(ValidationMessages.maxLength("متن نظر", 5));
+    }
+}
---
+++ Shop/Shop.Application/Comments/Edit/EditCommentCommand.cs
@@ -0,0 +1,6 @@
+﻿using Common.Application;
+using Shop.Domain.CommentAgg;
+
+namespace Shop.Application.Comments.Edit;
+
+public record EditCommentCommand(long CommentId,string Text , long UserId ) : IBaseCommand;
