8b5dfad91d0a0db7d07e2781a1a2835059187a80
Arhamepa <PC@DESKTOP-PQA26UF>
2024-12-29 09:29:18 +0330

    Seller Commands Is Done

---
+++ Shop/Shop.Application/Sellers/Create/CreateSellerCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using Common.Application.Validation;
+using Common.Application.Validation.FluentValidations;
+using FluentValidation;
+
+namespace Shop.Application.Sellers.Create;
+
+public class CreateSellerCommandValidator:AbstractValidator<CreateSellerCommand>
+{
+    public CreateSellerCommandValidator()
+    {
+        RuleFor(i => i.ShopName)
+            .NotEmpty().WithMessage(ValidationMessages.required("نام فروشگاه"));
+
+        RuleFor(n => n.NationalCode)
+            .NotEmpty().WithMessage(ValidationMessages.required("کد ملی"))
+            .ValidNationalId();
+    }
+}
---
+++ Shop/Shop.Application/Sellers/Edit/EditSellerCommand.cs
@@ -0,0 +1,4 @@
+﻿using Common.Application;
+
+namespace Shop.Application.Sellers.Edit; 
+public record EditSellerCommand(long Id,string ShopName,string NationalCode) : IBaseCommand;
