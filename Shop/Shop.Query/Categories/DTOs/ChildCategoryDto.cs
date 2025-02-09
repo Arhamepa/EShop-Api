b7304608feb5f3ca2424bc6d741df999bdcdb14e
Arhamepa <PC@DESKTOP-PQA26UF>
2025-02-09 13:30:18 +0330

    Add Category Queries And Create Common Query

---
+++ Shop/Shop.Query/Categories/DTOs/ChildCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using Common.Domain.ValueObjects;
+using Common.Query;
+using Shop.Domain.CategoryAgg;
+
+namespace Shop.Query.Categories.DTOs;
+
+public class ChildCategoryDto : BaseDto
+{
+    public string Title { get; set; }
+    public string Slug { get; set; }
+    public SeoData SeoData { get; set; }
+    public long? ParentId { get; set; }
+    public List<GrandChildCategoryDto> Children { get; set; }
+}
---
+++ Shop/Shop.Query/Categories/GetById/GetCategoryByIdQuery.cs
@@ -0,0 +1,26 @@
+﻿using Common.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.Infrastructure.Persistent.Ef;
+using Shop.Query.Categories.DTOs;
+
+namespace Shop.Query.Categories.GetById;
+
+public record GetCategoryByIdQuery(long Id) : IQuery<CategoryDto>;
+
+
+internal class GetCategoryByIdQueryHandler : IQueryHandler<GetCategoryByIdQuery, CategoryDto>
+{
+    private readonly ShopContext _context;
+
+    public GetCategoryByIdQueryHandler(ShopContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<CategoryDto> Handle(GetCategoryByIdQuery request, CancellationToken cancellationToken)
+    {
+        var model =await _context.Categories
+            .FirstOrDefaultAsync(f => f.Id == request.Id , cancellationToken);
+        return model.Map();
+    }
+}
